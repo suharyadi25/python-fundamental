0ff49f2cb9c3b5552ef15186c925386e3777fcc1
Suharyadi <suharyadi2004@gmail.com>
2022-11-13 16:30:24 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# sequential
+print('test')
+
+# percabangan
+milk_bottle = 125
+egg_count = 125
+print(f'sum bottle of milk {milk_bottle} ')
+print(f'sum egg {egg_count} ')
+
+if milk_bottle > 0:
+    print('1 buying milk')
+    if egg_count > 0:
+        print('buy 6 egg')
+    else:
+        print('dont buy egg')
+else:
+    print('dont buy milk')

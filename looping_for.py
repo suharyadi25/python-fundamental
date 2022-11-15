a323131080a792fbbc5fd5b8a094800114fb8836
suharyadi25 <suhayadi2004@gmail.com>
2022-11-15 09:41:57 +0700

    looping for and while

---
+++ looping_for.py
@@ -0,0 +1,10 @@
+sum_book = 10
+print('reading book')
+
+reading_book = 2
+print(f'sum reading book {reading_book}')
+
+for reading_book in range(reading_book+1, sum_book+1):
+    print(f'number book - {reading_book} done read')
+
+print(f'you done read book = {reading_book}')
---
+++ looping_while.py
@@ -0,0 +1,11 @@
+sum_book = 100
+print('reading a book')
+
+sum_reading_book = 56
+print(f'number done read {sum_reading_book}')
+
+while sum_reading_book < sum_book:
+    sum_reading_book = sum_reading_book + 1
+    print(f'number book - {sum_reading_book} done read')
+
+print(f'you done read book = {sum_reading_book}')
